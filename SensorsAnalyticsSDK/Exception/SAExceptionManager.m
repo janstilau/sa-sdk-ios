68e46ffeca1de7973a81736b8fa7a8750e72f263
liuguoqiang <liugq01@rd.netease.com>
2023-08-13 00:42:14 +0800

    mod: 看一看神策数据.

--- SensorsAnalyticsSDK/Exception/SAExceptionManager.m
+++ SensorsAnalyticsSDK/Exception/SAExceptionManager.m
@@ -1,23 +1,3 @@
-//
-// SAExceptionManager.m
-// SensorsAnalyticsSDK
-//
-// Created by 张敏超🍎 on 2021/6/4.
-// Copyright © 2015-2022 Sensors Data Co., Ltd. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-
 #if ! __has_feature(objc_arc)
 #error This file must be compiled with ARC. Either turn on ARC for the project or use -fobjc-arc flag on this file.
 #endif
@@ -61,10 +41,10 @@ static NSString * const kSAAppCrashedReason = @"app_crashed_reason";
 
 - (void)setEnable:(BOOL)enable {
     _enable = enable;
-
+    
     if (enable) {
         _prev_signal_handlers = calloc(NSIG, sizeof(struct sigaction));
-
+        
         [self setupExceptionHandler];
     }
 }
@@ -81,7 +61,7 @@ static NSString * const kSAAppCrashedReason = @"app_crashed_reason";
 - (void)setupExceptionHandler {
     _defaultExceptionHandler = NSGetUncaughtExceptionHandler();
     NSSetUncaughtExceptionHandler(&SAHandleException);
-
+    
     struct sigaction action;
     sigemptyset(&action.sa_mask);
     action.sa_flags = SA_SIGINFO;
@@ -110,10 +90,10 @@ static void SASignalHandler(int crashSignal, struct __siginfo *info, void *conte
         NSException *exception = [NSException exceptionWithName:kSASignalExceptionName
                                                          reason:reason
                                                        userInfo:userInfo];
-
+        
         [SAExceptionManager.defaultManager handleUncaughtException:exception];
     }
-
+    
     struct sigaction prev_action = SAExceptionManager.defaultManager.prev_signal_handlers[crashSignal];
     if (prev_action.sa_flags & SA_SIGINFO) {
         if (prev_action.sa_sigaction) {
@@ -131,7 +111,7 @@ static void SAHandleException(NSException *exception) {
     if (exceptionCount <= kSAExceptionMaximum) {
         [SAExceptionManager.defaultManager handleUncaughtException:exception];
     }
-
+    
     if (SAExceptionManager.defaultManager.defaultExceptionHandler) {
         SAExceptionManager.defaultManager.defaultExceptionHandler(exception);
     }
@@ -149,22 +129,22 @@ static void SAHandleException(NSException *exception) {
             properties[kSAAppCrashedReason] = [NSString stringWithFormat:@"%@ %@", exception.reason, [NSThread.callStackSymbols componentsJoinedByString:@"\n"]];
         }
         SAPresetEventObject *object = [[SAPresetEventObject alloc] initWithEventId:kSAEventNameAppCrashed];
-
+        
         [SensorsAnalyticsSDK.sharedInstance trackEventObject:object properties:properties];
-
+        
         //触发页面浏览时长事件
         [[SAModuleManager sharedInstance] trackPageLeaveWhenCrashed];
-
+        
         // 触发退出事件
         [SAModuleManager.sharedInstance trackAppEndWhenCrashed];
-
+        
         // 阻塞当前线程，完成 serialQueue 中数据相关的任务
         sensorsdata_dispatch_safe_sync(SensorsAnalyticsSDK.sdkInstance.serialQueue, ^{});
         SALogError(@"Encountered an uncaught exception. All SensorsAnalytics instances were archived.");
     } @catch(NSException *exception) {
         SALogError(@"%@ error: %@", self, exception);
     }
-
+    
     NSSetUncaughtExceptionHandler(NULL);
     signal(SIGABRT, SIG_DFL);
     signal(SIGILL, SIG_DFL);
